davidfoerster/DSA-4.1-Heldendokument

commit 515bfe2b57646feb51c04b5d39d9ade870e04009

Fix and simplify TP/KK melee modifier caculations
TP/KK modifiers for melee weapon damage, AT, and PA were incorrectly rounded
towards negative infinity instead of zero as required by the rule book.

The modifier calculation is now done using integer division rather than repeated
addition/subtraction.

src/kampfbogen.lua
@@ -34,10 +34,7 @@ local function atpa_mod(basis, talent, schwelle, schritt, wm, art, spez)
   local val = basis
   local cur_kk = data:cur("KK")
   if cur_kk ~= "" and schwelle ~= nil and schritt ~= nil and schritt > 0 then
-    while cur_kk < schwelle do
-      val = val - 1
-      cur_kk = cur_kk + schritt
-    end
+    val = val + common.div_trunc(cur_kk - schwelle, schritt)
   end
   if art ~= nil and spez ~= nil then
     for _, s in ipairs(spez) do
@@ -151,8 +148,7 @@ nahkampf_render[3]= {false, function(v, talent, ebe)
   end
 end}
 nahkampf_render[5]= {true, function(v)
-  local tp = common.schaden.parse(v)
-  common.schaden.render(tp)
+  common.schaden.render(common.schaden.parse(v))
 end}
 for i=8,10 do
   nahkampf_render[i] = {true, common.render_delta}

src/common.lua
@@ -426,6 +426,16 @@ function common.proviant_vermoegen()
   common.inner_rows(content, 4)
 end
 
+-- Truncating integer division
+function common.div_trunc(numerator, denominator)
+  local quotient = numerator // denominator
+  if quotient < 0 and numerator % denominator ~= 0 then
+    quotient = quotient + 1
+  end
+  return quotient
+end
+
+
 common.schaden = {}
 
 function common.schaden.parse(input)
@@ -501,14 +511,7 @@ function common.schaden.mod(tp, schwelle, schritt)
     return nil
   end
   if schwelle ~= nil and schritt ~= nil and schritt > 0 then
-    while cur_kk < schwelle do
-      tp.num = tp.num - 1
-      cur_kk = cur_kk + schritt
-    end
-    while cur_kk >= schwelle + schritt do
-      tp.num = tp.num + 1
-      cur_kk = cur_kk - schritt
-    end
+    tp.num = tp.num + common.div_trunc(cur_kk - schwelle, schritt)
   end
   return tp
 end

commit 6fe24cc136b3210541d98e7fb11b5d4550ffb982

fix AT/PA calc error introduced in 515bfe2

src/kampfbogen.lua
@@ -35,7 +35,9 @@ local function atpa_mod(basis, talent, schwelle, schritt, wm, art, spez)
   local val = basis
   local cur_kk = data:cur("KK")
   if cur_kk ~= "" and schwelle ~= nil and schritt ~= nil and schritt > 0 then
-    val = val + common.div_trunc(cur_kk - schwelle, schritt)
+    if cur_kk < schwelle then
+      val = val + common.div_trunc(cur_kk - schwelle, schritt)
+    end
   end
   if art ~= nil and spez ~= nil then
     for _, s in ipairs(spez) do