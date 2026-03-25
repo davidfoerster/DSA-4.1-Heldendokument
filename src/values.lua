2ec83c4b6b68792b8101f078fc823d5b9f953fff
David Foerster <davidfoerster@users.noreply.github.com>
2026-03-25 03:51:27 +0100

    cleanup: values getter_map:formula()

--- src/values.lua
+++ src/values.lua
@@ -110,14 +110,7 @@ function getter_map:formula(name)
   if vals.hide_formula then
     return ""
   end
-  local res = "("
-  for i,v in ipairs(vals) do
-    if i ~= 1 then
-      res = res .. "+"
-    end
-    res = res .. v
-  end
-  return res .. ")/" .. vals.div
+  return string.format("(%s)/%d", table.concat(vals, "+"), vals.div)
 end
 
 getter_map:reg("basic", "MU", "KL", "IN", "CH", "FF", "GE", "KO", "KK")
